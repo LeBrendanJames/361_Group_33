5fadf29fe39a319292baa1c469241adf37f256fc
LeBrendanJames <35370736+LeBrendanJames@users.noreply.github.com>
2018-11-17 09:56:34 -0600

    Initial Ballot Data Structure

---
+++ ballot.js
@@ -0,0 +1,107 @@
+/*
+class VotingItem {
+    variables:
+        name
+		array of options
+		choice
+
+    methods:
+		makeChoice();
+		getOptions();
+}
+*/
+class VotingItem {
+	constructor(name, options, choiceNum){
+		this.name = name;
+		this.options = options;
+		this._choice = choiceNum; // Variable is known as choice to anyone using the class, but is actually stored as _choice.
+								  // This means that access is only through getter/setter
+								  // So, VotingItem.choice actually calls the getter function and returns _choice
+								  // and VotingItem.choice = "jfjfj" calls the setter for _choice with argument "jfjfj"
+	}
+   
+	get choice(){
+		return this._choice;
+	}
+   
+	set choice(choiceNum){
+		this._choice = choiceNum;
+	}
+   
+	makeChoice(choiceNum){
+		this._choice = choiceNum;
+	}
+               
+               
+}
+ 
+ 
+/*
+class Ballot {
+	variables:
+		ballotName
+		username
+		array of voting items
+				   
+	methods:
+		addVotingItem(VotingItem);
+		getVotingItemByNum();
+		getAllVotingItems();
+		makeChoice(votingItemNum, choiceNum);
+}
+*/
+class Ballot {
+	constructor(ballotName, username){
+		this._ballotName = ballotName;
+		this._username = username;
+		this.votingItems = [];
+	}
+   
+	get ballotName(){
+		return this._ballotName;
+	}
+   
+	get username() {
+		return this._username;
+	}
+   
+	addVotingItem(VotingItem){
+		this.votingItems.push(VotingItem);
+	}
+   
+	getVotingItemByNum(num){
+		return this.votingItems[num];
+	}
+   
+	// For summary where we want to show all items at once
+	getAllVotingItems(){
+		return this.votingItems;
+	}
+   
+	makeChoice(votingItemNum, choiceNum){
+		if (votingItemNum < this.votingItems.length){
+			this.votingItems[votingItemNum].makeChoice(choiceNum);
+		} else {
+			return "Error: voting item " + votingItemNum + " does not exist.";
+		}
+	}
+                            
+}
+ 
+ 
+var choices = ["Bill Gates", "Will Gates", "William Gates"]; // Will be filled from database eventually
+firstItem = new VotingItem("firstItemName", choices, 0);
+secondItem = new VotingItem("secondItemName", choices, 0)
+ 
+ballot = new Ballot("Test Name", "Brendan1234");
+console.log(ballot);
+ 
+ballot.addVotingItem(firstItem);
+ballot.addVotingItem(secondItem);
+console.log(ballot.getVotingItemByNum(0));
+console.log(ballot.getVotingItemByNum(1));
+ 
+console.log("Before choice: " + (ballot.getVotingItemByNum(0)).choice);
+ballot.makeChoice(0, 1);
+console.log("After Choice: " + (ballot.getVotingItemByNum(0)).choice);
+console.log(ballot.getAllVotingItems());
