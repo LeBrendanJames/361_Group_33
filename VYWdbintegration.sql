902cf55d4479617a7a5c177939c0feccbd12e77f
mross444 <rossmic@oregonstate.edu>
2018-11-28 11:28:12 -0500

    more sample data added

--- VYWdbintegration.sql
+++ VYWdbintegration.sql
@@ -51,10 +51,10 @@ SELECT t1.questionID, t1.questionTitle, t1.questionURL, t2.responsePK, t2.respon
 FROM tblQuestion t1
 LEFT JOIN tblResponse t2 ON t1.questionPK = t2.questionFK
 LEFT JOIN tblVoterToResponse t3 ON t1.questionPK = t3.questionFK AND t3.voterFK = (SELECT voterFK FROM tblUserLogin WHERE username = 'matt')
-WHERE t1.electionFK = (SELECT electionPK FROM tblElection WHERE electionID = '2018MIDTERM')
+WHERE t1.electionFK = (SELECT electionPK FROM tblElection WHERE electionID = '2020PRES')
 
 -- Compact version
-SELECT t1.questionID, t1.questionTitle, t1.questionURL, t2.responsePK, t2.responseID, t2.responseTitle, t2.responseSubTitle, t3.responseFK, t3.writeInResponse FROM tblQuestion t1 LEFT JOIN tblResponse t2 ON t1.questionPK = t2.questionFK LEFT JOIN tblVoterToResponse t3 ON t1.questionPK = t3.questionFK AND t3.voterFK = (SELECT voterFK FROM tblUserLogin WHERE username = 'matt') WHERE t1.electionFK = (SELECT electionPK FROM tblElection WHERE electionID = '2018MIDTERM')
+SELECT t1.questionID, t1.questionTitle, t1.questionURL, t2.responsePK, t2.responseID, t2.responseTitle, t2.responseSubTitle, t3.responseFK, t3.writeInResponse FROM tblQuestion t1 LEFT JOIN tblResponse t2 ON t1.questionPK = t2.questionFK LEFT JOIN tblVoterToResponse t3 ON t1.questionPK = t3.questionFK AND t3.voterFK = (SELECT voterFK FROM tblUserLogin WHERE username = 'matt') WHERE t1.electionFK = (SELECT electionPK FROM tblElection WHERE electionID = '2020PRES')
 
 
 --Given an electionID and a specific questionID, get all of the questions response options, and existing user responses associated with that question.  This should accept questionID as a parameter from the site.
@@ -62,30 +62,30 @@ SELECT t1.questionID, t1.questionTitle, t1.questionURL, t2.responsePK, t2.respon
 FROM tblQuestion t1
 LEFT JOIN tblResponse t2 ON t1.questionPK = t2.questionFK
 LEFT JOIN tblVoterToResponse t3 ON t1.questionPK = t3.questionFK AND t3.voterFK = (SELECT voterFK FROM tblUserLogin WHERE username = 'matt')
-WHERE t1.electionFK = (SELECT electionPK FROM tblElection WHERE electionID = '2018MIDTERM') AND t1.QuestionID = '2018MID_Q1'
+WHERE t1.electionFK = (SELECT electionPK FROM tblElection WHERE electionID = '2020PRES') AND t1.QuestionID = '2020PRES_Q1'
 
 -- Compact version
-SELECT t1.questionID, t1.questionTitle, t1.questionURL, t2.responsePK, t2.responseID, t2.responseTitle, t2.responseSubTitle, t3.responseFK, t3.writeInResponse FROM tblQuestion t1 LEFT JOIN tblResponse t2 ON t1.questionPK = t2.questionFK LEFT JOIN tblVoterToResponse t3 ON t1.questionPK = t3.questionFK AND t3.voterFK = (SELECT voterFK FROM tblUserLogin WHERE username = 'matt') WHERE t1.electionFK = (SELECT electionPK FROM tblElection WHERE electionID = '2018MIDTERM') AND t1.QuestionID = '2018MID_Q1'
+SELECT t1.questionID, t1.questionTitle, t1.questionURL, t2.responsePK, t2.responseID, t2.responseTitle, t2.responseSubTitle, t3.responseFK, t3.writeInResponse FROM tblQuestion t1 LEFT JOIN tblResponse t2 ON t1.questionPK = t2.questionFK LEFT JOIN tblVoterToResponse t3 ON t1.questionPK = t3.questionFK AND t3.voterFK = (SELECT voterFK FROM tblUserLogin WHERE username = 'matt') WHERE t1.electionFK = (SELECT electionPK FROM tblElection WHERE electionID = '2020PRES') AND t1.QuestionID = '2020PRES_Q1'
 
 
 -- Register a new response to a question (select an option).  This should accept voterID or username, questionID, and responseID as paramaters from the site.
 INSERT INTO tblVoterToResponse (voterFK, questionFK, responseFK, writeInResponse)
   VALUES
-  ((SELECT voterFK FROM tblUserLogin WHERE username = 'matt'), (SELECT questionPK FROM tblQuestion WHERE questionID = '2018MID_Q1'), (SELECT responsePK FROM tblResponse WHERE responseID = '2018MID_Q1R1'), NULL)
+  ((SELECT voterFK FROM tblUserLogin WHERE username = 'matt'), (SELECT questionPK FROM tblQuestion WHERE questionID = '2020PRES_Q1'), (SELECT responsePK FROM tblResponse WHERE responseID = '2020PRES_Q1R1'), NULL)
 
 
 -- Register a new response to a question (write in response).  This should accept voterID or username, questionID, and writeInResponse as paramaters from the site.
 INSERT INTO tblVoterToResponse (voterFK, questionFK, responseFK, writeInResponse)
   VALUES
-  ((SELECT voterFK FROM tblUserLogin WHERE username = 'matt'), (SELECT questionPK FROM tblQuestion WHERE questionID = '2018MID_Q2'), NULL, 'Ross Perot')
+  ((SELECT voterFK FROM tblUserLogin WHERE username = 'matt'), (SELECT questionPK FROM tblQuestion WHERE questionID = '2020PRES_Q2'), NULL, 'Ross Perot')
 
 
 -- Change vote that was already saved (select an option) This should accept voterID or username, questionID, and responseID as paramaters from the site.
 UPDATE tblVoterToResponse
-SET    responseFK = (SELECT responsePK FROM tblResponse WHERE  responseID = '2018MID_Q1R2'), 
+SET    responseFK = (SELECT responsePK FROM tblResponse WHERE  responseID = '2020PRES_Q1R2'), 
        writeInResponse = NULL 
 WHERE  voterFK = (SELECT voterFK FROM tblUserLogin WHERE username = 'matt') 
-       AND questionFK = (SELECT questionPK FROM tblQuestion WHERE  questionID = '2018MID_Q1') 
+       AND questionFK = (SELECT questionPK FROM tblQuestion WHERE  questionID = '2020PRES_Q1') 
 
 
 -- Change vote that was already saved (writeInResponse) 
@@ -93,7 +93,7 @@ UPDATE tblVoterToResponse
 SET    responseFK = NULL,
        writeInResponse = 'Gordon Bombay'
 WHERE  voterFK = (SELECT voterFK FROM tblUserLogin WHERE username = 'matt') 
-       AND questionFK = (SELECT questionPK FROM tblQuestion WHERE  questionID = '2018MID_Q2') 
+       AND questionFK = (SELECT questionPK FROM tblQuestion WHERE  questionID = '2020PRES_Q2') 
 
 
 /************************************************************
